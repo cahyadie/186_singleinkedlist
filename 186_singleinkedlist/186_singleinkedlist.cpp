2984232d8e85cfe1e8adba6fae2071941f799dba
cahyadie <cahyadiilham12@gmail.com>
2023-05-18 12:24:38 +0700

    membuat void searchData

--- 186_singleinkedlist/186_singleinkedlist.cpp
+++ 186_singleinkedlist/186_singleinkedlist.cpp
@@ -105,3 +105,24 @@ void traverse() {
 		}
 	}
 }
+
+void searchData() {
+	if (listEmpty()) {
+		cout << "List Kosong" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	else {
+		int nim;
+		cout << "Maukan NIM : ";
+		cin >> nim;
+		node* currentNode = START;
+		while (currentNode != NULL){
+			if (currentNode->noMhs == nim) {
+				cout << "NIM : " << currentNode->noMhs << ",Nama : " << currentNode->name << endl;
+				return;
+			}
+		}
+	}
+}
