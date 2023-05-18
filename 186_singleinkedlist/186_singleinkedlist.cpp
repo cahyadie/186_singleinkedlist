c85fab81b2b38a88c1070868be4cd599f0e37e26
cahyadie <cahyadiilham12@gmail.com>
2023-05-18 11:56:53 +0700

    make void node

--- 186_singleinkedlist/186_singleinkedlist.cpp
+++ 186_singleinkedlist/186_singleinkedlist.cpp
@@ -8,3 +8,43 @@ struct node{
 };
 
 node* START = NULL;
+
+void addnode() {
+	int nim;
+	string nama;
+	node* nodeBaru = new node();
+	cout << "masukan NIM : ";
+	cin >> nim;
+	cout << "masukan Nama : ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+
+	node* previous = START;
+	node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
+}
