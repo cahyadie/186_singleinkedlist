09b98f488b60cb9342a5e05ac3a3545c4d36b90b
cahyadie <cahyadiilham12@gmail.com>
2023-05-18 12:33:44 +0700

    membuat case 2 untuk list kosong

--- 186_singleinkedlist/186_singleinkedlist.cpp
+++ 186_singleinkedlist/186_singleinkedlist.cpp
@@ -150,6 +150,14 @@ int main() {
 				system("pause");
 				system("cls");
 				break;
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List Kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
 			}
 		}
 	}
